567e51c9499a361d10c124fb4146e820bd677c1b
laljidp <yuvalprajapati572@gmail.com>
2023-03-06 16:02:00 +0530

    Initialized app boilerplate structure.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+const LoginPage = React.lazy(() => import('./components/Login'));
+const HomePage = React.lazy(() => import('./components/Home'));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<h1>Loading...</h1>}>
+              <LoginPage />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <React.Suspense fallback={<h1>Loading...</h1>}>
+              <HomePage />
+            </React.Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Container from '@mui/system/Container';
+
+function Home() {
+  return <Container>Home page</Container>;
+}
+
+export default Home;
